77b7de2909a13a971ea518481873fce02b5686a4
zlToms <253628393qq.com>
2021-01-03 23:32:41 +0800

    test02

--- src/main/java/com/tz/pool/test/Stest01.java
+++ src/main/java/com/tz/pool/test/Stest01.java
@@ -4,7 +4,7 @@ public class Stest01 {
 
     public static void main(String[] args) {
         System.out.println("dfjlkjdf");
-        System.out.println(12345);
+        System.out.println(123456);
     }
 }
   
--- src/main/java/com/tz/pool/test01/Test.java
+++ src/main/java/com/tz/pool/test01/Test.java
@@ -1,4 +1,7 @@
 package com.tz.pool.test01;
 
 public class Test {
+    public static void main(String[] args) {
+        
+    }
 }
